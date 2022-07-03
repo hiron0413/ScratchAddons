f0b5c81111f57f0ddc82a7caf476020b2abcf623
hiron0413 <hikan_behase@icloud.com>
2022-07-03 15:35:03 +0900

    update addon

--- addons/grid-list-of-costumes/userscript.js
+++ addons/grid-list-of-costumes/userscript.js
@@ -2,7 +2,7 @@ export default async function ({ addon, global, console, msg }) {
     function classNames(...names) {
         const _classNames = {
             defaultLayout: "sa-default-layout-button sa-costume-editor-layout-button",
-            tableLayout: "sa-table-layout-button sa-costume-editor-layout-button",
+            gridLayout: "sa-grid-layout-button sa-costume-editor-layout-button",
             toggledOff: "sa-costume-editor-layout-button-toggled-off"
         };
 
@@ -22,11 +22,13 @@ export default async function ({ addon, global, console, msg }) {
             element.style[keys[i]] = values[i]
         }
     }
+
+    let layout = "default"
     
     while (true) {
         let costumeListArea = await addon.tab.waitForElement(
             'div[class*="selector_list-area"]',
-            { markAsSeen: true }
+            { markAsSeen: false }
         );
 
         let costumeListWrapper = await addon.tab.waitForElement(
@@ -39,16 +41,24 @@ export default async function ({ addon, global, console, msg }) {
             { markAsSeen: true }
         );
 
-        let costumeSelectors = costumeListArea.childNodes;
+        if (layout === "grid") {
+            for (let element of costumeListArea.childNodes) {
+                style(element.firstChild, { margin: "0.5rem" })
+            }
+            console.log("a")
+        } else {
+            console.log(layout)
+        }
 
         const row = document.createElement("div");
         row.className = "sa-costume-editor-layout-row";
 
-        const tableLayout = document.createElement("span");
-        tableLayout.className = classNames("tableLayout", "toggledOff");
-        tableLayout.onclick = function() {
+        const gridLayout = document.createElement("span");
+        gridLayout.className = classNames("gridLayout", "toggledOff");
+        gridLayout.onclick = function() {
+            layout = "grid"
             defaultLayout.className = classNames("defaultLayout", "toggledOff");
-            tableLayout.className = classNames("tableLayout");
+            gridLayout.className = classNames("gridLayout");
             style(paintEditor, { display: "none" });
             style(costumeListWrapper, { 
                 width: "100%",
@@ -62,37 +72,36 @@ export default async function ({ addon, global, console, msg }) {
                 height: "auto",
                 padding: "0 0.5rem"
             });
-            for (let element of costumeSelectors) {
-                style(element.firstChild, { margin: "0.5rem" })
-            }
+
+            costumeListArea.classList.add("sa-costume-grid-list-area")
         }
         
         const defaultLayout = document.createElement("span");
         defaultLayout.className = classNames("defaultLayout")
         defaultLayout.onclick = function() {
+            layout = "default"
             defaultLayout.className = classNames("defaultLayout");
-            tableLayout.className = classNames("tableLayout", "toggledOff");
+            gridLayout.className = classNames("gridLayout", "toggledOff");
             style(paintEditor);
             style(costumeListWrapper);
             style(costumeListArea);
-            for (let element of costumeSelectors) {
-                style(element.firstChild)
-            }
+
+            costumeListArea.classList.remove("sa-costume-grid-list-area")
         }
 
-        const tableLayoutIcon = document.createElement("img");
-        tableLayoutIcon.className = "sa-table-layout-button-icon";
-        tableLayoutIcon.src = addon.self.dir + "/assets/table-layout.svg";
-        tableLayoutIcon.draggable = false;
+        const gridLayoutIcon = document.createElement("img");
+        gridLayoutIcon.className = "sa-grid-layout-button-icon";
+        gridLayoutIcon.src = addon.self.dir + "/assets/grid-layout.svg";
+        gridLayoutIcon.draggable = false;
 
         const defaultLayoutIcon = document.createElement("img");
         defaultLayoutIcon.className = "sa-default-layout-button-icon";
         defaultLayoutIcon.src = addon.self.dir + "/assets/default-layout.svg";
         defaultLayoutIcon.draggable = false;
 
-        tableLayout.appendChild(tableLayoutIcon);
+        gridLayout.appendChild(gridLayoutIcon);
         defaultLayout.appendChild(defaultLayoutIcon);
-        row.appendChild(tableLayout);
+        row.appendChild(gridLayout);
         row.appendChild(defaultLayout)
 
         costumeListArea.before(row);
